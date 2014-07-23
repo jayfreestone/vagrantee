d35ffb31dfd6f38e6d3dcc5330ed9f7e11e06525
Mitesh Shah <Mitesh.Shah@rtCamp.com>
2014-07-23 12:21:47 +0530

    Update EasyEngine Installation
    Ref: https://github.com/rtCamp/easyengine#quick-start

--- easyengine.sh
+++ easyengine.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 apt-get install -y curl
-curl -sL rt.cx/ee | sudo bash
+wget -qO ee rt.cx/ee && sudo bash ee 
 source /etc/bash_completion.d/ee
-ee system install
+ee stack install
 cat ~/.my.cnf
